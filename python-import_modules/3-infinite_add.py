73c05c248d359b72ef28665033353d1d6419a810
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-09 19:47:45 -0700

    Create 3-infinite_add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    result = 0
     argc = len(sys.argv)
     if argc == 1:
-        print("0 argument.")
+        print("0")
+        quit()
     else:
-        print(f"{argc - 1}" + " argument" + ("s:" if argc > 2 else ":"))
         for idx, arg in enumerate(sys.argv):
             if idx > 0:
-                print("{}: {}".format(idx, arg))
+                result += int(arg)
+    print("{}".format(result))
