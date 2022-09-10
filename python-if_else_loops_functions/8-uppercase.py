e359f42c81133f58427b1d1f24d352c8db9e3648
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-09 18:43:35 -0700

    Create uppercase function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
+def uppercase(c):
+    if ord(c) >= 65 and ord(c) <= 90:
         return True
     else:
         return False
