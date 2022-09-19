834fd548e82f7d9320ecc2121454bc77480e8c23
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-18 22:47:52 -0300

    Fix new_in_list function.

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None or idx in not range(0, len(my_list)):
+    if my_list is None or idx not in range(0, len(my_list)):
         return my_list
     new_list = my_list.copy()
     new_list[idx] = element
