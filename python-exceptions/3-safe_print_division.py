3e517c557d0fea6dc9a39518cb4741d0999d5d7c
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-18 23:50:45 -0300

    Create list division function.

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,6 +5,7 @@ def safe_print_division(a, b):
         result = a / b
         print("Inside result: {}".format(result))
     except ZeroDivisionError:
-        print("Inside result: None")
         result = None
-    return result
+        return result
+    finally:
+        return result
---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res_list = []
+    for i in range(0, list_length):
+        try:
+            res_list.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            res_list.append(0)
+        except TypeError:
+            print("wrong type")
+            res_list.append(0)
+        except IndexError:
+            print("out of range")
+            res_list.append(0)
+        finally:
+            continue
+    return res_list
