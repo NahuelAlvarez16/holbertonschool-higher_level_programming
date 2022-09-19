6364050318236268117411deb9fedc6727bc6c1d
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-18 22:46:03 -0300

    Fix new_in_list function.

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if !my_list or idx in range(0, len(my_list)):
+    if my_list is None or idx in range(0, len(my_list)):
         return my_list
     new_list = my_list.copy()
     new_list[idx] = element
