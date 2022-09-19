928312077e10b0f638e873509262b1f2245c32f4
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-18 23:45:50 -0300

    Create safe print division function.

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+        print("Inside result: {}".format(result))
+    except ZeroDivisionError:
+        print("Inside result: None")
+        result = None
+    return result
