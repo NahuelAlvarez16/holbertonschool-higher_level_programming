137c756076ba071982b93d737147b05e89d79366
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-23 20:44:47 -0300

    Fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,6 +7,6 @@ class Square:
     def __init__(self, size=0):
         if isinstance(size, int) is False:
             raise TypeError
-        if size >= 0:
+        if size < 0:
             raise ValueError
         self.__size = size
