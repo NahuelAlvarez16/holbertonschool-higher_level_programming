aa4976a6f9149dd0e42f3f020c233dc9aa903c49
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-23 20:41:59 -0300

    Create 3-quare.py file

---
+++ python-classes/3-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ Square """
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError
+        if size >= 0:
+            raise ValueError
+        self.__size = size
