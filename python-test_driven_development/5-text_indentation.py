091ff36ff1868fc7b01653e1abab3b36554f605e
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-24 20:42:26 -0300

    Add empty end to print in text_indentation function.

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,5 +6,5 @@ def text_indentation(text):
     """ function to print a text """
     if type(text) != str:
         raise TypeError("text must be a string")
-    print("{}".format(text.rstrip().lstrip()))
+    print("{}".format(text.rstrip().lstrip()), end="")
     
