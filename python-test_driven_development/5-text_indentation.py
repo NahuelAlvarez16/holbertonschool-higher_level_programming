4ba4420458cbd3a3c3f35422b1657737295a067e
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-24 20:55:04 -0300

    Fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,5 +6,8 @@ def text_indentation(text):
     """ function to print a text """
     if type(text) != str:
         raise TypeError("text must be a string")
-    print("{}".format(text.rstrip().lstrip()), end="")
+    for c in text:
+        print("{}".format(c), end="")
+        if c in ".:?":
+            print()
     
