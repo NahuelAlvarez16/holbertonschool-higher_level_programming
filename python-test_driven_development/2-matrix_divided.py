4f855b78e09a8efe24c5af5798a9f40ca7451d29
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-24 00:06:44 -0300

    FIx

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,18 +9,22 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     matrix_len = len(matrix[0])
     new_matrix = []
     for i, row in enumerate(matrix):
         if len(row) != matrix_len:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError(
+                    "Each row of the matrix must have the same size")
         matrix_len = len(row)
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         new_matrix.append([])
         for x in range(len(row)):
             if type(row[x]) != int and type(row[x]) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
             new_matrix[i].append(round(row[x] / float(div), 2))
     return new_matrix
