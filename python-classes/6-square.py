fc8b8854596dcd1d860e12b360b6af36a82fa2f1
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-23 21:42:59 -0300

    Add position attribute

---
+++ python-classes/6-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ Square """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if tuple.count(value) < 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        for i in range(self.size):
+            for i in range(self.size):
+                print("#", end="")
+            print()
+        if self.size == 0:
+            print()
