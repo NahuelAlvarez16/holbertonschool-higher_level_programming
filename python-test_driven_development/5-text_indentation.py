4dd44c39d085f6b49562c0feedf62c2616f8b5b9
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-24 20:32:11 -0300

    Create text_indentation function

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 5-text_indentation """
+
+
+def text_indentation(text):
+    """ function to print a text """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    print("{}".format(text.rtrip().strip()))
+    
