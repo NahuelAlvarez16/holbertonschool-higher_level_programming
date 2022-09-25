be45889cdb7a47cad859693e96841bdedc3f4363
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-24 21:28:56 -0300

    Fix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,17 +5,20 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-  def test_list(self):
-    self.assertEqual(max_integer([32, 12, 74, 12]), 74)
+    def test_list(self):
+        self.assertEqual(max_integer([32, 12, 74, 12]), 74)
 
-  def test_empty_list(self):
-    self.assertEqual(max_integer([]), None)
+    def test_max_at_beginning_list(self):
+        self.assertEqual(max_integer([42, 2, 7, 12]), 42)
 
-  def test_strings_list(self):
-    self.assertEqual(max_integer(['Hello', 'World']), 'World')
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
 
-  def test_one_item_list(self):
-    self.assertEqual(max_integer([12]), 12)
+    def test_strings_list(self):
+        self.assertEqual(max_integer(['Hello', 'World']), 'World')
+
+    def test_one_item_list(self):
+        self.assertEqual(max_integer([12]), 12)
 
 if __name__ == '__main__':
     unittest.main()
