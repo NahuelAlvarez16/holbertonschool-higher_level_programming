9986bf5a18bb3e0660d3cdd4ec9d5cbd6f5a80e2
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-24 21:22:57 -0300

    Create max_integer_test

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestMaxInteger(unittest.TestCase):
+
+  def test_list(self):
+    self.assertEqual(max_integer([32, 12, 74, 12]), 74)
+
+  def test_empty_list(self):
+    self.assertEqual(max_integer([]), None)
+
+  def test_strings_list(self):
+    self.assertEqual(max_integer(['Hello', 'World']), 'World')
+
+  def test_one_item_list(self):
+    self.assertEqual(max_integer([12]), 12)
+
+if __name__ == '__main__':
+    unittest.main()
