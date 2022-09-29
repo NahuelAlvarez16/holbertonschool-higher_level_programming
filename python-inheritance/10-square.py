059f31fdb124826ec2fec65ab01d9053dfd05b4d
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-28 23:25:31 -0300

    Fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" Square Module """
+""" Square module """
+
+
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
-    """ Square """
+    """ Square Class """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
