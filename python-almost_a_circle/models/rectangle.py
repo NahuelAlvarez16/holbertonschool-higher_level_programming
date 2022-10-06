8dbc497120975e0619a500485a8784d6ff954b1d
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-06 18:49:41 -0300

    Create validations in setters from rectangle class.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,14 +19,8 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        self.__width = value
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
         self.__width = value
 
     @property
@@ -35,6 +29,8 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
         self.__height = value
 
     @property
@@ -43,6 +39,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value == 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -51,4 +51,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value == 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
