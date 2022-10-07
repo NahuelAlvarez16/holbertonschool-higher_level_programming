2b7af5c7b6134e18931df30e499508d876106e9c
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-10-06 23:16:24 -0300

    Fixes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -65,11 +65,12 @@ class Base:
         This function creates a list of instances from a json file.
         """
         file_path = cls.__name__ + ".json"
-        if os.path.exists(file_path) is not exists:
+        if os.path.exists(file_path):
+            with open(file_path, 'w') as f:
+                obj = []
+                data = cls.from_json_string(f.read())
+                for d in data:
+                    obj.append(cls.create(d))
+                return obj
+        else:
             return []
-        with open(file_path, 'w') as f:
-            obj = []
-            data = cls.from_json_string(f.read())
-            for d in data:
-                obj.append(d)
-            return cls.create(obj)
