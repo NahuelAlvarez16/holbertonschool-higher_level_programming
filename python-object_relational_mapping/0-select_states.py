0c064b2dc0277ec406548ea8d352812c331ca994
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-07 18:04:25 -0300

    Fix 0-select_states python.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    result = cur.execute("SELECT * FROM states")
-    for row in result:
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
