e9684ab2ce1d761e4214a4c531fbd7f9507cf6fa
Nahuel Alvarez <nahuel.alvarez161@hotmail.com>
2022-11-06 22:50:40 -0300

    Fix 2-create_read_user sql.

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- Script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE USER IF NOT EXISTS 'hbtn_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
-SHOW GRANTS FOR hbtn_0d_2@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2 '@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2 '@'localhost';
