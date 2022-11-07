9cf55de467941972d4264db8110b56a4ef9e54b3
Nahuel Alvarez <nahuel.alvarez161@hotmail.com>
2022-11-06 23:57:13 -0300

    Create 16 task.

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_genres.name
+from tv_genres
+LEFT JOIN tv_show_genres on tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_shows on tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
