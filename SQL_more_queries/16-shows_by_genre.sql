82883a64c06a653e61d23d29b3ca8bd238477352
Nahuel Alvarez <nahuel.alvarez161@hotmail.com>
2022-11-06 23:59:04 -0300

    Fix

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_genres.name
 from tv_shows
 LEFT JOIN tv_show_genres on tv_show_genres.genre_id = tv_genres.id
-LEFT JOIN tv_genres on tv_show_genres.show_id = tv_genres.id
+LEFT JOIN tv_genres on tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name ASC;
