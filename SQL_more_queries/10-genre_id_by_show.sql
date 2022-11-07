6a08a4adb4e6061e831e78123a822af123a9384b
Nahuel Alvarez <nahuel.alvarez161@hotmail.com>
2022-11-06 23:19:51 -0300

    Create 10-genre_id_by_show sql.

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
