efff9b3a529046f5ced0ba6fe489c1c944595e63
Nahuel Alvarez <nahuel.alvarez161@hotmail.com>
2022-11-06 23:47:45 -0300

    Create 14-my_genres sql.

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- Script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT tv_genres.name FROM tv_genres, tv_shows, tv_show_genres
+WHERE tv_genres.id = tv_show_genres.genre_id
+AND tv_shows.id = tv_show_genres.show_id
+AND tv_shows = 'Dexter';
