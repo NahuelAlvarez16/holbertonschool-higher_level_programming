cc022f4761bd129d4b1fa60ef2d59647237d44ca
Nahuel Alvarez <nahuel.alvarez161@hotmail.com>
2022-11-06 23:11:22 -0300

    Fix 8-cities_of_california_subquery sql.

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id;
+SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id and states.name = 'California';
