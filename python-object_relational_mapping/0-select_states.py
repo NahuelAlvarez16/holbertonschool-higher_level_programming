6c79b36e43004053df1c86a9ea34ed3b6df55ef1
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-07 18:11:17 -0300

    Fix code style of 0-select_state.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-    Importing the MySQLdb module and the sys module. It is then connecting to the database and creating
-    a cursor. It is then executing the query and printing the results.
+    Importing the MySQLdb module and the sys module. It is then connecting to\
+        the database and creating a cursor. It is then executing the query and\
+            printing the results.
 """
 if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
