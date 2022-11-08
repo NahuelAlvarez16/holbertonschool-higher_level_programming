84b53b93c195409e8fc2235573cdadbdc852552f
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-08 17:52:03 -0300

    Fix 3-my_safe_filter_states

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE\
-        BINARY name = '%(name)s'", {
+        BINARY name = %(name)s", {
             'name': sys.argv[4]
         })
     rows = cur.fetchall()
