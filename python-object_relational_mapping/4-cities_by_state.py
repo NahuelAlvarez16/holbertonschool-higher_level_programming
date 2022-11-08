60a20bf284d2d8a2120d680e70ab4504ff60a4f9
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-08 17:56:56 -0300

    Fix 4-cities_by_state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities")
+    cur.execute("SELECT cities.id, cities.name, state.name FROM cities LEFT states ON states.id = cities.state_id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
