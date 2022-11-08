ba28aad591695c543d7804263c3ea9656fd35ec6
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-08 19:12:36 -0300

    Fix model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,5 +13,5 @@ class State(Base):
         State Class
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key = True)
-    name =  Column(String(128), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
