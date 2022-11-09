7efeef8c19a61b0c1188ceac39a3edd28f1b9484
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-08 22:54:12 -0300

    Fix code style 12-model_state_update...

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,11 +9,10 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import update
 
-
     engine = create_engine("mysql://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.query(State).filter(State.id == 2).update({"name":"New Mexico"})
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
     session.commit()
