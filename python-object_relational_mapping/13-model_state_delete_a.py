bc4c53745ba7eb9d1d9af2cb00beb98f5b0d6fce
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-09 12:27:50 -0300

    Fix 13-model_state_delete..

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker, aliased
     from model_state import Base, State
 
-    engine = create_engine("mysql://{}:{}@127.0.0.1/{}"
+    engine = create_engine("mysql://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
