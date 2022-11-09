26da3aa335e1ddf3f1cf64c56dab7d39c8de86fc
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-08 21:53:06 -0300

    Create 11-model_state_insert

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+    from sqlalchemy.orm import sessionmaker, aliased
+    from model_state import Base, State
+
+    engine = create_engine("mysql://{}:{}@127.0.0.1/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    newState = State(name='Louisiana')
+    session.add(newState)
+    session.flush()
+    session.refresh(newState)
+    print("{}".format(newState.id))
