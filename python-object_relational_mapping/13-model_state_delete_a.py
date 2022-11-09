a03d666bfeedc70608c064411fb246ae18447ce5
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-08 23:09:19 -0300

    Create 13-model_state...

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+    from sqlalchemy.orm import sessionmaker, aliased
+    from model_state import Base, State
+    from sqlalchemy import update
+
+    engine = create_engine("mysql://{}:{}@localhost{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like("%P%"))
+    session.delete(states)
+    session.commit()
