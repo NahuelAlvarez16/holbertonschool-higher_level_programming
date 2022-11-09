0e19f7dfd700f7b8303adff246757692ab72f4dd
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-11-08 21:08:28 -0300

    Create 10-model_state_my_get python.

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    import sys
+    from sqlalchemy.orm import sessionmaker, aliased
+    from model_state import Base, State
+
+    engine = create_engine("mysql://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name == sys.argv[4]):
+        print("{}: {}".format(state.id, state.name))
