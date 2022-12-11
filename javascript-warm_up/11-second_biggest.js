bbdc2cfe51c9b05f7bd5f2045629fb492ee1fcd8
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-11 18:19:56 -0300

    Fix second_biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-const argv = process.argv.slice(2);
+const argv = process.argv.slice(2).map((n) => {
+  return parseInt(n);
+});
 
 if (!argv[0] || isNaN(argv[1])) {
   console.log('0');
 } else {
-  console.log(argv.sort().reverse()[1]);
+  console.log(argv.sort((a, b) => {
+    return a - b;
+  }).reverse()[1]);
 }
