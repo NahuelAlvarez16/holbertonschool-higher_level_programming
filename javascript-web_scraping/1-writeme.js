1d6dbba7d9d09b6c29e53de27cf321528838dc2b
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-18 22:29:58 -0300

    Create 1-writeme.js

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argv = process.argv.slice(2);
+
+fs.writeFile(argv[0], argv[1], (error, data) => {
+  if (error) {
+    console.error(error);
+  }
+});
