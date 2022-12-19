38aa058aa29699bb7ef053952f699413d2148799
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-18 23:05:05 -0300

    Fix 5-request_store

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,8 +5,9 @@ const request = require('request');
 const argv = process.argv.slice(2);
 
 request.get(argv[0], (error, response, body) => {
-  if (error)
+  if (error) {
     console.log(error);
+  }
 
   fs.writeFile(argv[1], body, (error, data) => {
     if (error) {
