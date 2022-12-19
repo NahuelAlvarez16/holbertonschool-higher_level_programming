51cb3a11d3ec00142b08a1b3a168bea02b6899ba
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-18 22:24:37 -0300

    FIx 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,10 @@
 const fs = require('fs');
 const argv = process.argv.slice(2);
 
-fs.readFile('./' + argv[0], (err, data) => {
-  if (err) return console.error(err);
-  console.log(data.toString());
+fs.readFile('./' + argv[0], (error, data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(data);
 });
