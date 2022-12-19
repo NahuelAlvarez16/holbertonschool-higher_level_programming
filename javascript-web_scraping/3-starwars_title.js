afb5e7f9b7cd5cfd9591a26e8725d08a0e20d142
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-18 22:40:38 -0300

    Create 3-starwars_title.js

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv = process.argv.slice(2);
+
+request.get('https://swapi-api.hbtn.io/api/films/' + argv[0], (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
